f6098c5c93e95ab6077d2f8c24940b2ecd74f839
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2023-03-24 15:33:38 +0900

    mainscreen 디자인 수정 완료, BLE 통신 기능 구현 작업중

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.test
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,16 @@ class MainActivity : AppCompatActivity() {
 
         val Webview_btn: Button = findViewById(R.id.Webview_btn)
         val Wife_btn: Button = findViewById(R.id.Wifi_btn)
+
+        Wife_btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            val file = File("path/to/AI-thinker test 1.0.1")
+            val uri = Uri.fromFile(file)
+            intent.setDataAndType(uri, "application/vnd.android.package-archive")
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+        }
 
         Webview_btn.setOnClickListener {
             val intent = Intent(this, WebviewActivity::class.java)
