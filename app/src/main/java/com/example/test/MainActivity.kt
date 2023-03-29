590d47c4bbe39138c5ce9b9b7590ed029201cb0c
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2023-03-29 11:22:31 +0900

    manifest 수정

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.test
 
-import android.Manifest
-import android.bluetooth.BluetoothAdapter
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
