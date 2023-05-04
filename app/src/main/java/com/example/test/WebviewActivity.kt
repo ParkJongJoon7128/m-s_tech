2affb33e745b7095e30a5cff666955b00a967d89
parkjongjoon7128 <whdwnsdk8111@naver.com>
2023-05-04 17:59:33 +0900

    WebView 수정 작업중

--- app/src/main/java/com/example/test/WebviewActivity.kt
+++ app/src/main/java/com/example/test/WebviewActivity.kt
@@ -1,14 +1,15 @@
 package com.example.test
 
+import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
+import android.net.http.SslError
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.os.Message
+import android.view.WindowManager
+import android.webkit.*
 import java.net.URISyntaxException
 
 class WebviewActivity : AppCompatActivity() {
@@ -18,51 +19,61 @@ class WebviewActivity : AppCompatActivity() {
 
         val webview: WebView = findViewById(R.id.Webview)
 
-        webview.settings.javaScriptEnabled = true // 자바스크립트 허용
+        webview.apply {
+            // 사이트 로드
+            this.loadUrl("http://safewing.co.kr")
 
-        //웹뷰에서 새 창이 뜨지 않도록 방지하는 구문
-        webview.webChromeClient = WebChromeClient()
+            // 자바스크립트 허용
+            settings.javaScriptEnabled = true
 
-        //링크 주소를 Load 함
-        webview.loadUrl("http://safewing.co.kr")
+            // 자바스크립트의 window.open 허용
+            settings.javaScriptCanOpenWindowsAutomatically = true
 
-        webview.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                if (url == null) return false
-                if (!url.startsWith("http://") && !url.startsWith("https://")) {
-                    if (url.startsWith("intent")) {
-                        val schemeIntent: Intent = try {
-                            Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
-                        } catch (e: URISyntaxException) {
-                            e.printStackTrace()
-                            return false
-                        }
-                        try {
-                            startActivity(schemeIntent)
-                            return true
-                        } catch (e: ActivityNotFoundException) {
-                            val pkgName = schemeIntent.getPackage()
-                            if (pkgName != null) {
-                                startActivity(
-                                    Intent(
-                                        Intent.ACTION_VIEW,
-                                        Uri.parse("market://details?id=$pkgName")
-                                    )
-                                )
+            // 웹뷰에서 새 창이 뜨지 않도록 방지하는 구문
+            this.webChromeClient = WebChromeClient()
+
+            this.settings.domStorageEnabled = true
+            this.settings.setSupportMultipleWindows(true)
+
+            // 결제 시스템 사용 가능하게 기능 구현
+            this.webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    if (url == null) return false
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                        if (url.startsWith("intent")) {
+                            val schemeIntent: Intent = try {
+                                Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                            } catch (e: URISyntaxException) {
+                                e.printStackTrace()
+                                return false
+                            }
+                            try {
+                                startActivity(schemeIntent)
                                 return true
+                            } catch (e: ActivityNotFoundException) {
+                                val pkgName = schemeIntent.getPackage()
+                                if (pkgName != null) {
+                                    startActivity(
+                                        Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("market://details?id=$pkgName")
+                                        )
+                                    )
+                                    return true
+                                }
+                            }
+                        } else {
+                            return try {
+                                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                                true
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                                false
                             }
                         }
-                    } else {
-                        return try {
-                            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-                            true
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            false
-                        }
                     }
+                    return false
                 }
-                return false
             }
         }
     }
