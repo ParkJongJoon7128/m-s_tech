f5b3e57ca2083ce3361a44d0878c3e94f1d12b3f
parkjongjoon7128 <whdwnsdk8111@naver.com>
2023-06-26 10:43:56 +0900

    기능 추가 작업중

--- app/src/main/java/com/example/test/MainMenuChartFragment.kt
+++ app/src/main/java/com/example/test/MainMenuChartFragment.kt
@@ -1,4 +1,5 @@
 // MainMenuChartFragment.kt
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -6,6 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.test.R
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
@@ -19,21 +24,90 @@ class MainMenuChartFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_main_menu_chart, container, false)
         filterChart = view.findViewById(R.id.filter_chart)
+
+        initBarChart(filterChart)
         setupChart()
-        
+
         return view
+    }
+
+    private fun initBarChart(barChart: BarChart){
+        // 차트 회색 배경 설정 (default = false)
+        barChart.setDrawGridBackground(false)
+        // 막대 그림자 설정 (default = false)
+        barChart.setDrawBarShadow(false)
+        // 차트 테두리 설정 (default = false)
+        barChart.setDrawBorders(false)
+
+        val description = Description()
+        // 오른쪽 하단 모서리 설명 레이블 텍스트 표시 (default = false)
+        description.isEnabled = false
+        barChart.description = description
+
+        // X, Y 바의 애니메이션 효과
+        barChart.animateY(1000)
+        barChart.animateX(1000)
+
+        // 바텀 좌표 값
+        val xAxis: XAxis = barChart.xAxis
+        // x축 위치 설정
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        // 그리드 선 수평 거리 설정
+        xAxis.granularity = 1f
+        // x축 텍스트 컬러 설정
+        xAxis.textColor = Color.BLACK
+        // x축 선 설정 (default = true)
+        xAxis.setDrawAxisLine(false)
+        // 격자선 설정 (default = true)
+        xAxis.setDrawGridLines(false)
+
+        val leftAxis: YAxis = barChart.axisLeft
+        // 좌측 선 설정 (default = true)
+        leftAxis.setDrawAxisLine(false)
+        // 좌측 텍스트 컬러 설정
+        leftAxis.textColor = Color.BLACK
+
+        val rightAxis: YAxis = barChart.axisRight
+        // 우측 선 설정 (default = true)
+        rightAxis.setDrawAxisLine(false)
+        // 우측 텍스트 컬러 설정
+        rightAxis.textColor = Color.BLACK
+
+        // 바차트의 타이틀
+        val legend: Legend = barChart.legend
+        // 범례 모양 설정 (default = 정사각형)
+        legend.form = Legend.LegendForm.SQUARE
+        // 타이틀 텍스트 사이즈 설정
+        legend.textSize = 10f
+        // 타이틀 텍스트 컬러 설정
+        legend.textColor = Color.BLACK
+        // 범례 위치 설정
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        // 범례 방향 설정
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        // 차트 내부 범례 위치하게 함 (default = false)
+        legend.setDrawInside(true)
     }
 
     private fun setupChart() {
         val entries = listOf(
             BarEntry(1f, 2f),
-//            BarEntry(2f, 4f),
-//            BarEntry(3f, 6f),
-//            BarEntry(4f, 8f),
-//            BarEntry(5f, 10f)
+            BarEntry(2f, 4f),
+            BarEntry(3f, 6f),
+            BarEntry(4f, 8f),
+        )
+        val title = "필터 상태"
+
+
+        val dataSet = BarDataSet(entries, title)
+        dataSet.setColor(
+            Color.rgb(151,218,59),
+            Color.rgb(0,195,244),
+//            Color.rgb(192,243,42),
+//            Color.rgb(202,104,217)
         )
 
-        val dataSet = BarDataSet(entries, "Example Data")
         val data = BarData(dataSet)
 
         filterChart.data = data
