d762cc0e3438b26ed9c79798d232c895c4338bb5
parkjongjoon7128 <whdwnsdk8111@naver.com>
2023-06-28 09:43:29 +0900

    기능 추가 작업중

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -13,10 +13,6 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-//    private var bleGatt: BluetoothGatt? = null
-//    private val serviceUUID = UUID.fromString("55e405d2-af9f-a98f-e54a-7dfe43535355")
-//    private val characteristicUUID = UUID.fromString("16962447-c623-61ba-d94b-4d1e43535349")
-
     private val fragmentManager: FragmentManager = supportFragmentManager
     private val fragmentChart: MainMenuChartFragment = MainMenuChartFragment()
     private val fragmentBLE: MainMenuBLEFragment = MainMenuBLEFragment()
@@ -50,45 +46,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-//        val Ble_btn: Button = findViewById(R.id.Ble_btn)
-//        val Webview_btn: Button = findViewById(R.id.Webview_btn)
-////        val test_btn: Button = findViewById(R.id.test_btn)
-//
-//        Ble_btn.setOnClickListener {
-//            val intent = Intent(this, BleActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-//        Webview_btn.setOnClickListener {
-//            val intent = Intent(this, WebviewActivity::class.java)
-//            startActivity(intent)
-//        }
-
-//        test_btn.setOnClickListener {
-//            val intent = Intent(this, TestActivity::class.java)
-//            startActivity(intent)
-//        }
-
-//        val test_btn: Button = findViewById(R.id.test_btn)
-//        test_btn.setOnClickListener {
-//            val builder = AlertDialog.Builder(this)
-//            val dialogView = layoutInflater.inflate(R.layout.dialog_testmanager, null)
-//            val test_editText: EditText = dialogView.findViewById(R.id.test_editText)
-//
-//            builder.setView(dialogView).setPositiveButton("전송") { dialog, _ ->
-//                val test_text = test_editText.text.toString()
-//
-//                val result = test_text.toByteArray()
-//                val service = bleGatt?.getService(serviceUUID)
-//                val characteristic = service?.getCharacteristic(characteristicUUID)
-//
-//                characteristic?.value = result
-//                bleGatt?.writeCharacteristic(characteristic)
-//
-//                dialog.dismiss()
-//
-//            }.setNegativeButton("취소") { dialog, _ ->
-//                dialog.dismiss()
-//            }.show()
-//        }
--- app/src/main/java/com/example/test/MainMenuWebViewFragment.kt
+++ app/src/main/java/com/example/test/MainMenuWebViewFragment.kt
@@ -25,11 +25,9 @@ class MainMenuWebViewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        configureWebView()
+//        configureWebView()
         loadWebViewUrl("http://safewing.co.kr") // 원하는 URL로 변경하세요.
-    }
 
-    private fun configureWebView() {
         webView.apply {
             // 자바스크립트 허용
             settings.javaScriptEnabled = true
@@ -115,6 +113,92 @@ class MainMenuWebViewFragment : Fragment() {
         }
     }
 
+//    private fun configureWebView() {
+//        webView.apply {
+//            // 자바스크립트 허용
+//            settings.javaScriptEnabled = true
+//
+//            // 자바스크립트의 window.open 허용
+//            settings.javaScriptCanOpenWindowsAutomatically = true
+//
+//            // 새 창 띄우기 허용
+//            settings.setSupportMultipleWindows(true)
+//
+//            // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+//            settings.loadWithOverviewMode = true
+//
+//            // 화면 사이즈 맞추기 허용여부
+//            settings.useWideViewPort = true
+//
+//            // 결제 시스템 사용 가능하게 기능 구현
+//            webViewClient = object : WebViewClient() {
+//                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+//                    if (url == null) return false
+//                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+//                        if (url.startsWith("intent")) {
+//                            val schemeIntent: Intent = try {
+//                                Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+//                            } catch (e: Exception) {
+//                                e.printStackTrace()
+//                                return false
+//                            }
+//                            try {
+//                                startActivity(schemeIntent)
+//                                return true
+//                            } catch (e: ActivityNotFoundException) {
+//                                val pkgName = schemeIntent.getPackage()
+//                                if (pkgName != null) {
+//                                    startActivity(
+//                                        Intent(
+//                                            Intent.ACTION_VIEW,
+//                                            Uri.parse("market://details?id=$pkgName")
+//                                        )
+//                                    )
+//                                    return true
+//                                }
+//                            }
+//                        } else {
+//                            return try {
+//                                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+//                                true
+//                            } catch (e: Exception) {
+//                                e.printStackTrace()
+//                                false
+//                            }
+//                        }
+//                    }
+//                    return false
+//                }
+//            }
+//
+//            webChromeClient = object : WebChromeClient() {
+//                override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+//                    val newWebView = WebView(requireContext()).apply {
+//                        webViewClient = WebViewClient()
+//                        settings.javaScriptEnabled = true
+//                    }
+//
+//                    val dialog = Dialog(requireContext()).apply {
+//                        setContentView(newWebView)
+//                        window!!.attributes.width = ViewGroup.LayoutParams.MATCH_PARENT
+//                        window!!.attributes.height = ViewGroup.LayoutParams.MATCH_PARENT
+//                        show()
+//                    }
+//
+//                    newWebView.webChromeClient = object : WebChromeClient() {
+//                        override fun onCloseWindow(window: WebView?) {
+//                            dialog.dismiss()
+//                        }
+//                    }
+//
+//                    (resultMsg?.obj as WebView.WebViewTransport).webView = newWebView
+//                    resultMsg?.sendToTarget()
+//                    return true
+//                }
+//            }
+//        }
+//    }
+
     private fun loadWebViewUrl(url: String) {
         webView.loadUrl(url)
     }
@@ -128,7 +212,6 @@ class MainMenuWebViewFragment : Fragment() {
         super.onPause()
         onBackPressedCallback.remove()
     }
-
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
             if (webView.canGoBack()) {
