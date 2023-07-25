f98f8a525dacd80e829c0d399d206f624b82d00c
parkjongjoon7128 <whdwnsdk8111@naver.com>
2023-07-25 16:38:07 +0900

    기능 추가 작업중

--- app/src/main/java/com/example/test/MainMenuSocketFragment.kt
+++ app/src/main/java/com/example/test/MainMenuSocketFragment.kt
@@ -2,29 +2,34 @@ package com.example.test
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import java.io.DataInputStream
-import java.io.DataOutputStream
 import java.net.ServerSocket
+import java.net.Socket
 
 
 class MainMenuSocketFragment : Fragment() {
 
-    private lateinit var serverBtn: Button
+    private lateinit var serverStartBtn: Button
+    private lateinit var serverEndBtn: Button
+    private var server: ServerSocket? = null // Declare the ServerSocket as nullable
+    private var socket: Socket? = null
+
 
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        val view =  inflater.inflate(R.layout.fragment_main_menu_socket, container, false)
+        val view = inflater.inflate(R.layout.fragment_main_menu_socket, container, false)
 
-        serverBtn = view.findViewById(R.id.serverBtn)
+        serverStartBtn = view.findViewById(R.id.serverStartBtn)
+        serverEndBtn = view.findViewById(R.id.serverEndBtn)
 
         return view
     }
@@ -32,26 +37,33 @@ class MainMenuSocketFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        serverBtn.setOnClickListener {
+        serverStartBtn.setOnClickListener {
 
-            Thread() {
+            Thread {
                 try {
-                    val server = ServerSocket(7128)
-                    println("사용자 접속 대기중...")
+                    server = ServerSocket(7128)
+                    Log.d("wifi_ip_test", "서버를 열었습니다.")
+                    Log.d("wifi_ip_test", "사용자 접속 대기중...")
 
-                    val socket = server.accept()
+                    server?.close()
+                    socket?.close()
 
-                    val input = socket.getInputStream()
-                    val dataInputStream = DataInputStream(input)
+//                    socket?.let {
+//                        val input = it.getInputStream()
+//                        val dataInputStream = DataInputStream(input)
+//                        val result = dataInputStream.readUTF()
+//                        Log.d("wifi_ip_test", "단말기로 부터 받은 값: $result")
+//                    }
 
-                    val stringData = dataInputStream.readUTF()
-
-                    println("Android 에서 받은 문자열 : $stringData")
-
-                    socket.close()
-                    server.close()
-
-
+//                    val input = socket.getInputStream()
+//                    val dataInputStream = DataInputStream(input)
+//
+//                    val result = dataInputStream.readUTF()
+//
+//                    println("단말기로 부터 받은 값: $result")
+//
+//                    socket.close()
+//                    server.close()
 
 //                    val output = socket.getOutputStream()
 //                    val dataOutputStream = DataOutputStream(output)
@@ -59,6 +71,22 @@ class MainMenuSocketFragment : Fragment() {
 //                    dataOutputStream.writeInt(7)
 //                    dataOutputStream.writeUTF("서버 문자열")
 
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+
+
+
+        serverEndBtn.setOnClickListener {
+            Thread {
+                try {
+                    socket?.close()
+                    server?.close()
+
+                    server = null // Set the server to null after closing
+                    Log.d("wifi_ip_test", "서버를 닫았습니다.")
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
