69701dcd8502216ec5b8bdf1039aba667787f01e
parkjongjoon7128 <whdwnsdk8111@naver.com>
2023-07-26 13:52:42 +0900

    기능 추가 작업중

--- app/src/main/java/com/example/test/MainMenuSocketFragment.kt
+++ app/src/main/java/com/example/test/MainMenuSocketFragment.kt
@@ -17,7 +17,7 @@ class MainMenuSocketFragment : Fragment() {
 
     private lateinit var serverStartBtn: Button
     private lateinit var serverEndBtn: Button
-    private var server: ServerSocket? = null // Declare the ServerSocket as nullable
+    private var server: ServerSocket? = null
     private var socket: Socket? = null
 
 
@@ -44,40 +44,11 @@ class MainMenuSocketFragment : Fragment() {
                     server = ServerSocket(7128)
                     Log.d("wifi_ip_test", "서버를 열었습니다.")
                     Log.d("wifi_ip_test", "사용자 접속 대기중...")
-
-//                    server?.close()
-//                    socket?.close()
-
-//                    socket?.let {
-//                        val input = it.getInputStream()
-//                        val dataInputStream = DataInputStream(input)
-//                        val result = dataInputStream.readUTF()
-//                        Log.d("wifi_ip_test", "단말기로 부터 받은 값: $result")
-//                    }
-
-//                    val input = socket.getInputStream()
-//                    val dataInputStream = DataInputStream(input)
-//
-//                    val result = dataInputStream.readUTF()
-//
-//                    println("단말기로 부터 받은 값: $result")
-//
-//                    socket.close()
-//                    server.close()
-
-//                    val output = socket.getOutputStream()
-//                    val dataOutputStream = DataOutputStream(output)
-//
-//                    dataOutputStream.writeInt(7)
-//                    dataOutputStream.writeUTF("서버 문자열")
-
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }.start()
         }
-
-
 
         serverEndBtn.setOnClickListener {
             Thread {
